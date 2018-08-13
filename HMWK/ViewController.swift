7ba469688371a5332640e2c1d59ab863feca489a
vincentius2020 <vmlewis2010@gmail.com>
2018-08-13 08:26:22 -0500

    Initial Commit

---
+++ HMWK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HMWK
+//
+//  Created by Vincent Lewis on 8/13/18.
+//  Copyright © 2018 HMWK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
